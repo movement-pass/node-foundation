4a6aa797b9886312dd51c78f3bb70e39df314efd
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-08-23 18:55:11 +0600

    chore: added external deployment script

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+﻿$profile = "movement-pass"
+$app = "movement-pass"
+$version = "v1"
+
+$names = @('configuration', 'certificates', 'jwt', 'photos', 'database')
+
+cdk bootstrap --profile $profile
+
+foreach ($name in $names) {
+    cdk deploy $app-$name-$version --require-approval never --profile $profile
+}
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/sh
+
+profile="movement-pass"
+app="movement-pass"
+version="v1"
+
+# shellcheck disable=SC2039
+names=("configuration" "certificates" "jwt" "photos" "database")
+
+cdk bootstrap --profile $profile
+
+# shellcheck disable=SC2039
+for name in "${names[@]}"
+do
+  cdk deploy "$app-$name-$version" --require-approval never --profile $profile
+done
