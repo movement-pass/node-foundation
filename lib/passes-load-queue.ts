b639e53ff1df5486b3d41393601649e9e45a9f2b
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-09-13 13:10:17 +0600

    chore: added sqs fifo queue for passes load

---
+++ lib/passes-load-queue.ts
@@ -0,0 +1,27 @@
+import { Construct, Duration, StackProps } from '@aws-cdk/core';
+
+import {
+  DeduplicationScope,
+  FifoThroughputLimit,
+  Queue
+} from '@aws-cdk/aws-sqs';
+
+import { Base } from './base';
+
+class PassesLoadQueue extends Base {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new Queue(this, 'Queue', {
+      queueName: `${this.app}_passes_load_${this.version}.fifo`,
+      receiveMessageWaitTime: Duration.seconds(20),
+      visibilityTimeout: Duration.minutes(5),
+      fifo: true,
+      contentBasedDeduplication: true,
+      fifoThroughputLimit: FifoThroughputLimit.PER_MESSAGE_GROUP_ID,
+      deduplicationScope: DeduplicationScope.MESSAGE_GROUP
+    });
+  }
+}
+
+export { PassesLoadQueue };
